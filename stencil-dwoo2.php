94be112e4c35ee62665faa0c2ac0fd3c2733b8ae
jip laptop <jhp.moors@gmail.com>
2016-01-02 20:48:02 +0100

    preparation for in-theme installation - without breaking plugin installed implementation

--- stencil-dwoo2.php
+++ stencil-dwoo2.php
@@ -1,27 +1,27 @@
 <?php
 /**
-Plugin Name: Stencil: Dwoo 2 Implementation
-Plugin URI: https://github.com/moorscode/stencil/
-Description: Dwoo 2 Stencil Implementation. This plugin enables the use of Dwoo 2 in your theme. This implementation requires the plugin "Stencil" to be installed and active.
-Version: 1.0.0
-Author: Jip Moors (moorscode)
-Author URI: http://www.jipmoors.nl
-Text Domain: stencil
-License: GPL2
-
-Stencil - Dwoo 2 (beta) addon is free software: you can redistribute it
-and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 2 of the
-License, or any later version.
-
-Stencil - Dwoo 2 (beta) addon is distributed in the hope that it will
-be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Stencil - Dwoo 2 (beta).
-If not, see http://www.gnu.org/licenses/gpl-2.0.html.
+ * Plugin Name: Stencil: Dwoo 2 Implementation
+ * Plugin URI: https://github.com/moorscode/stencil/
+ * Description: Dwoo 2 Stencil Implementation. This plugin enables the use of Dwoo 2 in your theme. This implementation requires the plugin "Stencil" to be installed and active.
+ * Version: 1.0.0
+ * Author: Jip Moors (moorscode)
+ * Author URI: http://www.jipmoors.nl
+ * Text Domain: stencil
+ * License: GPL2
+ *
+ * Stencil - Dwoo 2 (beta) addon is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 2 of the
+ * License, or any later version.
+ *
+ * Stencil - Dwoo 2 (beta) addon is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stencil - Dwoo 2 (beta).
+ * If not, see http://www.gnu.org/licenses/gpl-2.0.html.
  *
  * @package Stencil
  * @subpackage Dwoo2
@@ -33,40 +33,44 @@ if ( ! function_exists( 'add_filter' ) ) {
 	exit();
 }
 
-include( 'stencil-dependency.php ' );
+if ( ! function_exists( '__stencil_dwoo2_register' ) ) :
 
-add_filter( 'stencil:register-engine', '__stencil_dwoo2_register' );
+	include( 'stencil-dependency.php ' );
 
-/**
- * Register this implementation to Stencil
- *
- * @param array $engines Currently registered engines.
- *
- * @return array
- */
-function __stencil_dwoo2_register( $engines ) {
-	$engine    = 'Dwoo 2';
-	$engines[] = $engine;
+	add_filter( 'stencil:register-engine', '__stencil_dwoo2_register' );
 
-	add_action( 'stencil.activate-' . $engine, '__stencil_dwoo2_activate' );
+	/**
+	 * Register this implementation to Stencil
+	 *
+	 * @param array $engines Currently registered engines.
+	 *
+	 * @return array
+	 */
+	function __stencil_dwoo2_register( $engines ) {
+		$engine    = 'Dwoo 2';
+		$engines[] = $engine;
 
-	return $engines;
-}
+		add_action( 'stencil.activate-' . $engine, '__stencil_dwoo2_activate' );
 
-/**
- * On activation, include engine file.
- */
-function __stencil_dwoo2_activate() {
-	include_once( 'stencil-engine.php' );
-}
+		return $engines;
+	}
 
-/**
- * Load plugin textdomain
- */
-add_action(
-	'plugins_loaded',
-	create_function(
-		'',
-		"load_plugin_textdomain( 'stencil', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );"
-	)
-);
+	/**
+	 * On activation, include engine file.
+	 */
+	function __stencil_dwoo2_activate() {
+		include_once( 'stencil-engine.php' );
+	}
+
+	/**
+	 * Load plugin textdomain
+	 */
+	add_action(
+		'plugins_loaded',
+		create_function(
+			'',
+			"load_plugin_textdomain( 'stencil', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );"
+		)
+	);
+
+endif;
